a4e7a11df63d68feba66565a59a140d7250b9b2f
Mohannad Abu Ajameih <ajameih@yahoo.com>
2024-06-29 01:34:39 +0300

    final

--- .local/install/CodedWorkflow.cs
+++ .local/install/CodedWorkflow.cs
@@ -19,7 +19,7 @@ namespace REF_Assignment3_Performer
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService)};
+            _ = new System.Type[]{typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Testing.API.ITestingService)};
         }
 
         protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>() ; }
